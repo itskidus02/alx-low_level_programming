51ca06f025a88fd47d5a8d119d1a751249795d7b
itskidus02 <kidusasebe2019@gmail.com>
2023-09-06 02:50:04 -0700

    d

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,3 @@
+VimCrypt~03!à¹ÞJ|Ôj»¥M4ù–Ãú/BO¸CHô$H;¼5‹Õ¡£ˆbì¡›Ð,Êåêz‡D©P¤è›ê•ó+±_—5•»ù%jx¯^÷HA¶Â]`­
+tÿ4!\Ê58r¤6`+0y‡‰'\
+³ÿ(ƒŽËïË‰jã¢Ô{>_¸xôÈ·€8ýQ¿D'ð˜û{¯Öõ×hãžQ6W|ÝÏcH}ã-Å4Ñú›"QXwã“ñ|Ê5X{‡š1PnÕ„É”e&ˆ09aKããÚ'`TCr$ˆq=\¢âKPÛáÃ)Êüîf\à[•kOJËõæƒ&ý¿OSñ`ÚÂúzQÞqÊG*‚ÿLæWëB$¸mÚW­`2X;œ «Íx.ö€òw†W3ökænKöØ%]ñt ·¦‰ÿÍ_àW!o‡
